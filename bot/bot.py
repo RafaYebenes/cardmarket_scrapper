d9e4b3e367a1f6c6d9d44aa6ba4716cd06ead33f
RafaYebenes <terenrafa@hotmail.com>
2025-06-20 21:54:53 +0200

    add api flask

--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,8 @@ from telegram.ext import (
 import re
 from bot.handlers_service import handle_button_click, handle_message
 from db.db_service import insert_user, check_if_user_exists
-
+from flask import Flask
+app = Flask('')
 # Reemplaza con tu token
 TOKEN = "7194405151:AAHdeuhVtwmokePq0sEdJQwMR7dRY8CxR3U"
 
@@ -32,6 +33,13 @@ def main():
 
     print("Bot funcionando... Pulsa Ctrl+C para detenerlo.")
     app.run_polling()
+    
+@app.route('/')
+def home():
+    return "Bot activo"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
     main()
